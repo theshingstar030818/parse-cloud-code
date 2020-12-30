5d7890c926e2c3717a597eadcc8907b433ca8159
Tanzeel Rana <tanzeelrana@cmail.carleton.ca>
2020-12-30 13:23:53 -0500

    Update hello.js

--- cloud/hello.js
+++ cloud/hello.js
@@ -1,3 +1,12 @@
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+// Parse.Cloud.define('hello', function(req, res) {
+//   res.success('Hi');
+// });
+
+Parse.Cloud.define("hello", async (request) => {
+  console.log("hellooooo called");
+  return "123123123";
 });
+
+Parse.Cloud.afterSave(Parse.User, async (request) => {
+  // code here
+})
